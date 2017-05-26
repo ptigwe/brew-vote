915a0f59aa4878269f8ba74050fb661eaf97c258
Tobenna P. Igwe <ptigwe@gmail.com>
2017-05-26 18:29:15 +0100

    Implemented basic rating form with verification

--- brew_vote.py
+++ brew_vote.py
@@ -1,10 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return "Hello World!!"
+    return render_template('layout.html')
+
+@app.route('/theme')
+def theme():
+    return render_template('theme.html')
+
+@app.route('/login')
+def login():
+    return render_template('signin.html')
+
+@app.route('/rate/<comp_id>')
+def rate(comp_id):
+    names = {'Appearance': 10, 'Finish': 20, 'Aroma': 10, 'Taste': 30, 'Drinkability': 30}
+    beers = [dict(zip(['id'] + list(names.keys()), [i + 1] + [0] * len(names))) for i in range(5)]
+    return render_template('comp.html', comp_id=comp_id, beers=beers, names=names.keys(), limit=names)
 
 if __name__ == '__main__':
     app.run()
