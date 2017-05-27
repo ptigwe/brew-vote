831805d085350a73b4a496c8076f4697904ed2d9
Tobenna P. Igwe <ptigwe@gmail.com>
2017-05-27 11:36:11 +0100

    Add support for sqlite and competition

--- brew_vote.py
+++ brew_vote.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
+from database import db_session
+import model
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -8,9 +11,33 @@ def get_scoring():
 def get_beers(comp_id, names):
     return [dict(zip(['id'] + list(names.keys()), [i + 1] + [0] * len(names))) for i in range(5)]
 
+def get_competitions():
+    comps = db_session.query(model.Competition).all()
+    for i in comps:
+        print(i.id)
+    return comps
+
+def get_competition(comp_id):
+    return db_session.query(model.Competition).filter(model.Competition.id == comp_id).first()
+
+def get_comp_beers(comp_id):
+    return db_session.query(model.Beer).filter(model.Beer.competition_id == comp_id).all()
+
+def generate_competition(name, beers):
+    comp = model.Competition(name)
+    comp.completed = False
+    db_session.add(comp)
+    for i in range(beers):
+        b = model.Beer()
+        b.competition = comp
+        db_session.add(b)
+    db_session.commit()
+    return comp
+
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    comps = get_competitions()
+    return render_template('index.html', comps=comps)
 
 @app.route('/theme')
 def theme():
@@ -20,11 +47,38 @@ def theme():
 def login():
     return render_template('signin.html')
 
-@app.route('/rate/<comp_id>')
-def rate(comp_id):
+@app.route('/comp/new', methods=['GET','POST'])
+def new_comp():
+    if request.method == 'POST':
+        name = request.form['name']
+        beers = request.form['beers']
+        comp = generate_competition(name, int(beers))
+        return redirect(url_for('view_comp', comp_id=comp.id))
+    return render_template('new_comp.html')
+
+@app.route('/comp/end/<comp_id>')
+def end_comp(comp_id):
+    comp = get_competition(comp_id)
+    comp.completed = True
+    db_session.add(comp)
+    db_session.commit()
+    return redirect(url_for('view_comp', comp_id=comp_id))
+
+@app.route('/comp/view/<comp_id>')
+def view_comp(comp_id):
+    comp = get_competition(comp_id)
+    beers = get_comp_beers(comp_id)
+    return render_template('view_comp.html', comp=comp, beers=beers)
+
+@app.route('/comp/rate/<comp_id>')
+def rate_comp(comp_id):
     names = get_scoring()
-    beers = get_beers(comp_id, names)
+    beers = get_comp_beers(comp_id)
     return render_template('comp.html', comp_id=comp_id, beers=beers, names=names.keys(), limit=names)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
     app.run()
---
+++ model.py
@@ -0,0 +1,40 @@
+import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Competition(Base):
+    __tablename__ = 'competition'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120))
+    completed = Column(Boolean, default=False)
+    comp_date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, name=None):
+        self.name = name
+        self.comp_date = datetime.datetime.utcnow()
+        if name is None or name == '':
+            self.name = self.name_from_date(self.comp_date)
+
+    def name_from_date(self, date):
+        return date.strftime("%B-%Y")
+
+class Beer(Base):
+    __tablename__ = 'beer'
+    id = Column(Integer, primary_key=True)
+    brewer = Column(String(120))
+    name = Column(String(120))
+    style = Column(String(120))
+    competition_id = Column(Integer, ForeignKey('competition.id'))
+    competition = relationship(Competition)
+
+class Voting(Base):
+    __tablename__ = 'voting'
+    id = Column(Integer, primary_key=True)
+    beer_id = Column(Integer, ForeignKey('beer.id'))
+    beer = relationship(Beer)
+    Appearance = Column(Integer)
+    Finish = Column(Integer)
+    Aroma = Column(Integer)
+    Taste = Column(Integer)
+    Drinkability = Column(Integer)
