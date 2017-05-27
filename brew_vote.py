5b754379a38d56b9cbfb14cf3c5016b51c8b5939
Tobenna P. Igwe <ptigwe@gmail.com>
2017-05-27 02:53:02 +0100

    Allow for collapsable panels

--- brew_vote.py
+++ brew_vote.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+def get_scoring():
+    return {'Appearance': 10, 'Finish': 20, 'Aroma': 10, 'Taste': 30, 'Drinkability': 30}
+
+def get_beers(comp_id, names):
+    return [dict(zip(['id'] + list(names.keys()), [i + 1] + [0] * len(names))) for i in range(5)]
+
 @app.route('/')
 def index():
     return render_template('layout.html')
@@ -16,8 +22,8 @@ def login():
 
 @app.route('/rate/<comp_id>')
 def rate(comp_id):
-    names = {'Appearance': 10, 'Finish': 20, 'Aroma': 10, 'Taste': 30, 'Drinkability': 30}
-    beers = [dict(zip(['id'] + list(names.keys()), [i + 1] + [0] * len(names))) for i in range(5)]
+    names = get_scoring()
+    beers = get_beers(comp_id, names)
     return render_template('comp.html', comp_id=comp_id, beers=beers, names=names.keys(), limit=names)
 
 if __name__ == '__main__':
